777c40d0bf41ffda66d1f1652c7e1b45f0da09c1
Adam Romlein <romleiaj@clarkson.edu>
2019-10-22 16:31:48 -0400

    Added teleop launch for base station..

--- src/runscripts/base_station/setup.sh
+++ src/runscripts/base_station/setup.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
-# NOTE: Must call "source" to setup environmental variables
+#NOTE: Must call "source" to set variables
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/../../../devel/setup.bash
-export ROS_MASTER_URI=$(cat ../ROS_MASTER.txt)
-echo "ROS_MASTER_URI is '${ROS_MASTER_URI}'."
-export ROS_IP="192.168.119.15"
-echo "ROS_IP is ${ROS_IP}."
+export ROS_MASTER_URI="http://192.168.1.5:11311"
+#echo "ROS_MASTER_URI is '${ROS_MASTER_URI}'."
+export ROS_IP="192.168.1.5"
+#echo "ROS_IP is ${ROS_IP}."
