5a39ebfd857916d2b47ce09309673c1776fd276f
Adam Romlein <romleiaj@clarkson.edu>
2019-10-22 16:29:08 -0400

    Added multimaster as submodule.

--- src/runscripts/jetson/open_tunnel.sh
+++ src/runscripts/jetson/open_tunnel.sh
@@ -1,4 +1,3 @@
 sudo openvpn --mktun --dev tun1
 sudo ifconfig tun1 up
-echo "pacman\nP@cm@n2019!" | \
-    sudo openconnect vpn.clarkson.edu --interface=tun1
+sudo openconnect vpn.clarkson.edu --interface=tun1
