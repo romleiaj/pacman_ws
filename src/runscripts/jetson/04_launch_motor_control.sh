dcd4beb15cd57a5a0ba7b8637d185eedab160a74
Adam Romlein <romleiaj@clarkson.edu>
2019-10-22 16:40:53 -0400

    Organization of py files in motor controller package.

--- src/runscripts/jetson/04_launch_motor_control.sh
+++ src/runscripts/jetson/04_launch_motor_control.sh
@@ -3,7 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source "${DIR}/setup.sh"
 
-roslaunch --wait pacman_launch diff_motor_control.launch \
-    port:="/dev/ttyACM0" \
-    baud:="115200" \
-    open-loop:="false"
+roslaunch --wait pacman_launch motor_controller.launch
