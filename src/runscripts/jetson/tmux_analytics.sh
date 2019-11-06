b53da567fbdeb0b38e21b4519d7df1b6f67e4389
Adam Romlein <romleiaj@clarkson.edu>
2019-11-06 12:25:48 -0500

    Added imu orientation tools.

--- src/runscripts/jetson/tmux_analytics.sh
+++ src/runscripts/jetson/tmux_analytics.sh
@@ -17,5 +17,9 @@ sleep 1
 tmux new-window -t $SESSION:2 -n "OdomFusion"
 tmux send-keys -t $SESSION:2 "source $DIR/11_odom_fusion.sh" C-m
 
+sleep 1
+tmux new-window -t $SESSION:3 -n "IMUOrientation"
+tmux send-keys -t $SESSION:3 "source $DIR/12_imu_orientation.sh" C-m
+
 # Keep it alive so we can kill it through supervisor
 sleep infinity
