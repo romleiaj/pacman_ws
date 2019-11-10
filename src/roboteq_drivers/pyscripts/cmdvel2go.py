c475fec0a5bef010d9c2bbe488e927154d55ead2
Adam Romlein <romleiaj@clarkson.edu>
2019-11-10 14:10:53 -0500

    Added heartbeat for controller.

--- src/roboteq_drivers/pyscripts/cmdvel2go.py
+++ src/roboteq_drivers/pyscripts/cmdvel2go.py
@@ -14,7 +14,8 @@ RPM_PER_GO = MAX_WHEEL_RPM / 1000.
 RADS_PER_GO = (RPM_PER_GO*2*np.pi)/60.
 
 def cmdvel2go(data):
-    global pub
+    global pub, last
+    last = rospy.Time.now()
     v = data.linear.x
     w = data.angular.z
 
@@ -39,12 +40,23 @@ def cmdvel2go(data):
 
 def main():
     rospy.init_node("cmdvel2GO")
-    global pub
+    global pub, last
     pub_topic = rospy.get_param("~pub_topic", default="roboteq_cmd")
     pub = rospy.Publisher(pub_topic, Int32MultiArray, queue_size=10)    
+    last = rospy.Time.now()
+
     rospy.Subscriber("/cmd_vel", Twist, cmdvel2go)
     
+    while not rospy.is_shutdown(): # heartbeat
+        if (rospy.Time.now() - last).to_sec() > 0.5:
+            rospy.logwarn("No input detected on /cmd_vel, stopping motors.")
+            msg = Int32MultiArray()
+            msg.data = [0, 0]
+            pub.publish(msg)
+        rospy.sleep(0.1)
+            
     rospy.loginfo("Waiting for messages on /cmd_vel...")
+
     rospy.spin()
 
 if __name__ == "__main__":
