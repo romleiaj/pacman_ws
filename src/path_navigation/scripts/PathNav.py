04273e13dd818f2cdd4122b2e7a0cb0828f47019
Adam Romlein <romleiaj@clarkson.edu>
2019-11-12 14:47:52 -0500

    Decently working odometry with excellent point following.

--- src/path_navigation/scripts/PathNav.py
+++ src/path_navigation/scripts/PathNav.py
@@ -2,9 +2,8 @@
 import rospy
 from geometry_msgs.msg  import Twist, Vector3
 from nav_msgs.msg import Odometry
-from math import atan2, sqrt, pi
+from math import atan2, sqrt, pi, cos, copysign
 from pacman_msgs.msg import PointArray
-from PID import PID
 
 import numpy as np
 import tf
@@ -22,13 +21,12 @@ class PathNavigation():
         self.point_subscriber = rospy.Subscriber("/path_points", PointArray, 
                 self.point_queue_callback)
         self.queue = []
-        self.rate = rospy.Rate(50)
-        #self.ang_vel = PID()
+        self.rate = rospy.Rate(30)
         
         # User specified tolerance and gains
         self.distance_tolerance = 0.1
-        self.lin_vel = .1 #.2 worked
-        self.Kp_w = .1
+        self.lin_vel = 0.1 #.2 worked
+        self.Kp_w = .4
 
     #Callback function implementing the odom value received
     def odom_callback(self, data):
@@ -82,8 +80,6 @@ class PathNavigation():
         return distance
 
     def move2point(self):
-        pose = self.odom.pose.pose.position
-
         try:
             goal_point = self.get_next_point()
         except Queue.Empty:
@@ -92,28 +88,35 @@ class PathNavigation():
         goal_pose_x = goal_point.x # x value
         goal_pose_y = goal_point.y # y value
         vel_msg = Twist()
-        
-        while self.get_distance(goal_pose_x, goal_pose_y) >= self.distance_tolerance:
+
+        while self.get_distance(goal_pose_x, goal_pose_y) >= self.distance_tolerance\
+                and not rospy.is_shutdown():
             orient = self.odom.pose.pose.orientation
+            pose = self.odom.pose.pose.position
             [roll, pitch, yaw] = tf.transformations.euler_from_quaternion([orient.x, orient.y, orient.z, orient.w])
             theta = atan2(goal_pose_y - pose.y, goal_pose_x - pose.x)
             
             #Porportional Controller
             
-            #if self.in_op:
-            #linear velocity in the x-axis:
             turn_angle = pi/2
             turn_factor = (turn_angle - abs(theta - yaw)) / turn_angle
+            turn_angle = 1
+            turn_factor = ((turn_angle - abs(cos(theta) - 
+                    cos(yaw))) / turn_angle)
             if turn_factor < 0:
+                rospy.logwarn("No forward velocity")
                 turn_factor = 0
+            if turn_factor > 1:
+                turn_factor = 1
             vel_msg.linear.x = self.lin_vel * turn_factor
-                #print(vel_msg.linear.x)
             #angular velocity in the z-axis: Add Differential
-            vel_msg.angular.z = self.Kp_w * (theta - yaw)
-            #else: # Last element of queue
-                #vel_msg.linear.x = 0
-                #vel_msg.angular.z =0
-                #vel_msg.linear.x = lin_vel * sqrt(pow((goal_pose_x - pose.x), 2) + pow((goal_pose_y - pose.y), 2))
+            delta_theta = theta - yaw
+            if delta_theta > pi:
+                delta_theta = (delta_theta - 2*pi)
+            elif delta_theta < -pi:
+                delta_theta = (delta_theta + 2*pi)
+            vel_msg.angular.z = (self.Kp_w * sqrt(abs(delta_theta)) * 
+                copysign(1, delta_theta))
                 
             #Publishing our vel_msg
             self.velocity_publisher.publish(vel_msg)
