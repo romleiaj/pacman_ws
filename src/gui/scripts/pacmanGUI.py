b8a438599fa7d8378e7befd2d48ba36e4dddc4d8
Adam Romlein <romleiaj@clarkson.edu>
2019-12-08 18:46:18 -0500

    Warped funky image displayed through ROS.

--- src/gui/scripts/pacmanGUI.py
+++ src/gui/scripts/pacmanGUI.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 from PyQt5 import QtWidgets, uic, QtCore, QtGui
 import sys
+import cv2
 import threading
+import numpy as np
 
 
 class GUI_API_Wrapper:
@@ -19,8 +21,6 @@ class MainWindowController:
 
         def getResourceFolder(self):
                 return ""
-
-
                         
 class _MainWindow(QtWidgets.QMainWindow):
         def __init__(self, controller):
@@ -30,26 +30,35 @@ class _MainWindow(QtWidgets.QMainWindow):
                 self.controller = controller
                 self.EStopBtn.clicked.connect(self.toggleEStop)
                 self.active = False
+                print("SHOWING WINDOW")
                 self.show()
                 
         def toggleEStop(self):
                 self.active = not self.active
                 if not self.active:
-                        self.EStopBtn.setStyleSheet("background-color: rgb(78, 154, 6);")
+                    self.EStopBtn.setStyleSheet("background-color: rgb(78, 154, 6);")
                 else:
-                        self.EStopBtn.setStyleSheet("background: red")
+                    self.EStopBtn.setStyleSheet("background: red")
                 self.controller.toggleEStop(self.active)
 
+        def displayImageRaw(self, img):
+            #h, w, _ = img.shape
+            w = self.openGLWidget_5.width()
+            h = self.openGLWidget_5.height()
+            rz_img = cv2.resize(img, (w, h))
+            cv2.imwrite("/home/adam/really.png", rz_img)
+
+            qimg = QtGui.QImage(rz_img, w, h, QtGui.QImage.Format_RGB888)
+            self.openGLWidget_5.setPixmap(QtGui.QPixmap(qimg))
+            self.openGLWidget_5.show()
 
 # creates the main window; sets the main window of controller
 def initGUI(controller):
-        GUI_API_Wrapper.app = QtWidgets.QApplication(sys.argv)
-        mainWindow = _MainWindow(controller)
-        controller.setMainWindow(controller)
+    GUI_API_Wrapper.app = QtWidgets.QApplication(sys.argv)
+    mainWindow = _MainWindow(controller)
+    controller.setMainWindow(mainWindow)
 
 # opens and runs the UI
 def runGUI():
-        print(GUI_API_Wrapper.app)
-        return GUI_API_Wrapper.app.exec_()
-
-        
+    print(GUI_API_Wrapper.app)
+    return sys.exit(GUI_API_Wrapper.app.exec_())
--- src/gui/scripts/rosController.py
+++ src/gui/scripts/rosController.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python
 import threading
+import cv2
+from math import atan2, sqrt, pi
+import tf
 import rospy
 from geometry_msgs.msg  import Twist, Vector3
 from nav_msgs.msg import Odometry
-from math import atan2, sqrt, pi
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
 import pacmanGUI
-
-import tf
-
 
 
 class RosController(pacmanGUI.MainWindowController):
     def __init__(self):
         rospy.init_node('GUI', anonymous=True)
+        self.bridge = CvBridge()
+        self.sub = rospy.Subscriber("/webcam/image_raw", Image, self.displayImg)
 
     def getResourceFolder(self):
-        return rospy.get_param("~resourceFilePath", "~/grobots/pacman_ws/src/pacman_config/pacman.ui")
+        return rospy.get_param("~resourceFilePath", 
+                "~/grobots/pacman_ws/src/pacman_config/pacman.ui")
     
     def closeWindow(self):
         self.getMainWindow().close()
@@ -27,11 +32,16 @@ class RosController(pacmanGUI.MainWindowController):
     def toggleEStop(self, active):
         print(active)
 
-    
+    def displayImg(self, img):
+        try:
+            cvimg = self.bridge.imgmsg_to_cv2(img, "rgb8")
+        except CvBridgeError as e:
+            print(e)
+        self.getMainWindow().displayImageRaw(cvimg)
 
 if __name__ == '__main__':
     rosCon = RosController()
     pacmanGUI.initGUI(rosCon)
-    rospy
+    #rospy
     pacmanGUI.runGUI()
-    
+    rospy.spin() 
