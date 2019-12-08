8ea6cc84dd0fce716cb2478f35e4276901d1a8e9
MassiCuts <maxcutugno16@gmail.com>
2019-12-08 16:47:54 -0500

    Added test file for GUI

--- src/gui/scripts/pacmanGUI.py
+++ src/gui/scripts/pacmanGUI.py
@@ -4,7 +4,7 @@ import sys
 import threading
 
 
-class _GUI_API_Wrapper:
+class GUI_API_Wrapper:
         app = None
 
 class MainWindowController:
@@ -27,28 +27,29 @@ class _MainWindow(QtWidgets.QMainWindow):
                 super(_MainWindow, self).__init__()
                 resourceFolder = controller.getResourceFolder()
                 uic.loadUi("%s/pacman.ui" % (resourceFolder), self)
-		self.EStopBtn.clicked.connect(self.toggleEStop)
-		self.controller = controller
-		self.active = False
+                self.controller = controller
+                self.EStopBtn.clicked.connect(self.toggleEStop)
+                self.active = False
                 self.show()
                 
-	def toggleEStop(self):
-		self.active = not self.active
-		if not self.active:
-			self.EStopBtn.setStyleSheet("background-color: rgb(78, 154, 6);")
-		else:
-			self.EStopBtn.setStyleSheet("background: red")
-		self.controller.toggleTStop(self.active)
+        def toggleEStop(self):
+                self.active = not self.active
+                if not self.active:
+                        self.EStopBtn.setStyleSheet("background-color: rgb(78, 154, 6);")
+                else:
+                        self.EStopBtn.setStyleSheet("background: red")
+                self.controller.toggleEStop(self.active)
 
 
 # creates the main window; sets the main window of controller
 def initGUI(controller):
-        _GUI_API_Wrapper.app = QtWidgets.QApplication(sys.argv)
+        GUI_API_Wrapper.app = QtWidgets.QApplication(sys.argv)
         mainWindow = _MainWindow(controller)
         controller.setMainWindow(controller)
 
 # opens and runs the UI
 def runGUI():
-        return _GUI_API_Wrapper.app.exec_()
+        print(GUI_API_Wrapper.app)
+        return GUI_API_Wrapper.app.exec_()
 
         
